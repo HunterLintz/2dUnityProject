7c4d17c2af0eec2c5a9f000779d290295b7bdb61
Zac Bagley <zbagley610@gmail.com>
2020-04-06 07:42:09 -0400

    Update playerMovement.cs For Mouse Control
    Figuring out calculations and how to set sprite based on mouse position

--- 2dUnityProject/Assets/playerMovement.cs
+++ 2dUnityProject/Assets/playerMovement.cs
@@ -6,55 +6,82 @@ public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    public Animator animator;
+    public Camera cam;
+    public Animator anim;
 
     Vector2 movement;
-
-		public void Start()
-		{
-			
-
-		}
+    Vector2 mousePos;
 
     // Update is called once per frame
     public void Update()
     {
         // Input
-
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-
         movement = movement.normalized;
+
+        //face mouse direction START
+        float deltaX = mousePos.x - transform.position.x;
+        float deltaY = mousePos.y - transform.position.y;
+
+        if (!(deltaX == 0f && deltaY == 0f))
+        {
+            if (deltaX > 0f && Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+            {
+                //right
+                anim.SetInteger("Horizontal", 1);
+            }
+            else if (deltaX < 0f && Mathf.Abs(deltaX) > Mathf.Abs(deltaY))
+            {
+                //left
+                anim.SetInteger("Horizontal", -1);
+            }
+            else if (deltaY > 0f && Mathf.Abs(deltaY) > Mathf.Abs(deltaX))
+            {
+                //up
+                anim.SetInteger("Vertical", 1);
+            }
+            else if (deltaY < 0f && Mathf.Abs(deltaY) > Mathf.Abs(deltaX))
+            {
+                //down
+                anim.SetInteger("Vertical", -1);
+            }
+            else
+            {
+                Debug.Log("faceMouseDir Error");
+            }
+        }
+        //face mouse direction END
 
         if (movement.x != 0)
         {
-            animator.SetBool("Is_Moving_Side", true);
+            anim.SetBool("Is_Moving_Side", true);
         }
         else
         {
-            animator.SetBool("Is_Moving_Side", false);
+            anim.SetBool("Is_Moving_Side", false);
         }
 
         if (movement != Vector2.zero)
         {
-            animator.SetFloat("Horizontal", movement.x);
-            animator.SetFloat("Vertical", movement.y);
-            animator.SetFloat("Speed", movement.sqrMagnitude);
-            animator.SetFloat("HorizontalIdle", movement.x);
-            animator.SetFloat("VerticalIdle", movement.y);
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Speed", movement.sqrMagnitude);
+            anim.SetFloat("HorizontalIdle", movement.x);
+            anim.SetFloat("VerticalIdle", movement.y);
         }
         else
         {
-            animator.SetFloat("Horizontal", 0);
-            animator.SetFloat("Vertical", 0);
-            animator.SetFloat("Speed", 0);
+            anim.SetFloat("Horizontal", 0);
+            anim.SetFloat("Vertical", 0);
+            anim.SetFloat("Speed", 0);
         }  
+        
     }
 
     public void FixedUpdate()
     {
         // Movement
-
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 				
     }
