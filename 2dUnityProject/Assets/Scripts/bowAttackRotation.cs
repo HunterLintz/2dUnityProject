f44e0661c98b4e4a7457000ad394259600598b97
Hunter Lintz <hunterlintz126@gmail.com>
2020-04-07 02:44:52 -0400

    Fixed bowAttackRotation script to only run when in bow mode and fixed attack input
    Fixed bowAttackRotation script to only run when in bow mode and fixed attack input

---
+++ 2dUnityProject/Assets/Scripts/bowAttackRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowAttackRotation : MonoBehaviour
+{
+	public Transform orb;
+	public float radius;
+	private Transform pivot;
+	public bool isBow;
+	public GameObject player;
+	public void Start()
+	{
+			pivot = orb.transform;
+			transform.parent = pivot;
+			transform.position += Vector3.up * radius;
+			player = GameObject.Find("Player");
+	}
+
+	public void FixedUpdate()
+	{
+		isBow = player.GetComponent<playerMovement>().isBow;
+
+		if (isBow == true)
+		{
+		Vector3 orbVector = Camera.main.WorldToScreenPoint(orb.position);
+		orbVector = Input.mousePosition - orbVector;
+		float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+
+		pivot.position = orb.position;
+		pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+		}
+	}
+}
