aff1f29655c5987d2286bfcc686ec5ba3a477f27
Zac Bagley <zbagley610@gmail.com>
2020-04-07 03:48:59 -0400

    added Arrow Hit Effect
    added Arrow Hit Effect

---
+++ 2dUnityProject/Assets/arrowScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrowScript : MonoBehaviour
+{
+ 
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision) 
+    {
+        GameObject effect =Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(effect,5f);
+        Destroy(gameObject);
+    }
+
+}
