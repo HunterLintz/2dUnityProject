61f39e60a8e52f7af183be41fcdc8dd4990f157a
Hunter Lintz <hunterlintz126@gmail.com>
2020-04-07 01:31:23 -0400

    Added Arrow Image and target that follows mouse when in "Bow Mode"
    Added Arrow Image and target that follows mouse when in "Bow Mode"

--- 2dUnityProject/Assets/Scripts/PlayerAttack.cs
+++ 2dUnityProject/Assets/Scripts/PlayerAttack.cs
@@ -8,74 +8,123 @@ public class PlayerAttack : MonoBehaviour
 	public Rigidbody2D rb;
 	public float attackTime;
 	public float startTimeAttack;
-	playerMovement move;
+	public bool isBow;
+	public Vector2 mousePos;
 	public Transform attackLocation;
 	public float attackRange;
 	public LayerMask enemies;
+	private GameObject targetObject;
+	private bool targetSpawned = false;
 	private float x;
 	private float y;
+	public GameObject target;
 
 	public void Start()
 	{
 		animator = GetComponent<Animator>();
 
 		rb = GetComponent<Rigidbody2D>();
-
-		move = GetComponent<playerMovement>();
 	}
 
 	public void FixedUpdate()
 	{
-		//checks if attack timer is done and then if the press the attack button set start the timer
-		if (attackTime == 0)
-		{
-			if (Input.GetButton("Fire1"))
-			{
-				animator.SetBool("Is_attacking", true);
-				attackTime = startTimeAttack;
-			}
+		isBow = GetComponent<playerMovement>().isBow;
+		mousePos = GetComponent<playerMovement>().mousePos;
+
+		if (isBow == false && targetSpawned == true)
+		{		
+				Cursor.visible = true;
+				Destroy(targetObject);
+				targetSpawned = false;
 		}
-		//timer starts. when attack time is less than .33 destroy any objects in attackLocation collider
-		if (attackTime > 0)
+		if (isBow == false)
 		{
-			attackTime -= Time.deltaTime;
-			if (attackTime <= .33)
+				//checks if attack timer is done and then if the press the attack button set start the timer
+			if (attackTime == 0)
 			{
-				Collider2D[] damage = Physics2D.OverlapCircleAll(attackLocation.position, attackRange, enemies);
-				for (int i = 0; i < damage.Length; i++)
+				if (Input.GetButton("Fire1"))
 				{
-						Destroy(damage[i].gameObject);
+					animator.SetBool("Is_attacking", true);
+					attackTime = startTimeAttack;
 				}
 			}
-			//sets attacking bool to false once attack timer is back to zero
-			if (attackTime <= 0)
-			{	
-				attackTime = 0;
-				animator.SetBool("Is_attacking", false);	
+			//timer starts. when attack time is less than .33 destroy any objects in attackLocation collider
+			if (attackTime > 0)
+			{
+				attackTime -= Time.deltaTime;
+				if (attackTime <= .33)
+				{
+					Collider2D[] damage = Physics2D.OverlapCircleAll(attackLocation.position, attackRange, enemies);
+					for (int i = 0; i < damage.Length; i++)
+					{
+							Destroy(damage[i].gameObject);
+					}
+				}
+				//sets attacking bool to false once attack timer is back to zero
+				if (attackTime <= 0)
+				{	
+					attackTime = 0;
+					animator.SetBool("Is_attacking", false);	
+				}
 			}
+			//adjusts melee attackLocations position based on movement
+			x = Input.GetAxisRaw("Horizontal");
+			y = Input.GetAxisRaw("Vertical");
+			//right
+			if (x > 0)
+			{
+					attackLocation.position = new Vector2(rb.position.x + (float)0.45, rb.position.y - (float).25);
+			}
+			//left
+			if (x < 0)
+			{
+					attackLocation.position = new Vector2(rb.position.x -(float)0.45, rb.position.y - (float).25);
+			}
+			//up
+			if (y > 0 && x == 0)
+			{
+				attackLocation.position = new Vector2(rb.position.x, rb.position.y + (float)0.25);
+			}
+			//down
+			if (y < 0 && x == 0)
+			{
+				attackLocation.position = new Vector2(rb.position.x, rb.position.y - (float)0.75);
+			}			
 		}
-		//adjusts melee attackLocations position based on movement
-		x = Input.GetAxisRaw("Horizontal");
-		y = Input.GetAxisRaw("Vertical");
-		//right
-		if (x > 0)
+		if (isBow == true && targetSpawned == false)
 		{
-				attackLocation.position = new Vector2(rb.position.x + (float)0.45, rb.position.y - (float).25);
+			targetObject = Instantiate(target,mousePos,Quaternion.identity);
+			targetSpawned = true;
+			Cursor.visible = false;
 		}
-		//left
-		if (x < 0)
+		if (isBow == true)
 		{
-				attackLocation.position = new Vector2(rb.position.x -(float)0.45, rb.position.y - (float).25);
-		}
-		//up
-		if (y > 0 && x == 0)
-		{
-			attackLocation.position = new Vector2(rb.position.x, rb.position.y + (float)0.25);
-		}
-		//down
-		if (y < 0 && x == 0)
-		{
-			attackLocation.position = new Vector2(rb.position.x, rb.position.y - (float)0.75);
+				targetObject.transform.position = new Vector2 (mousePos.x,mousePos.y);
+
+			//checks if attack timer is done and then if the press the attack button set start the timer
+			if (attackTime == 0)
+			{
+				if (Input.GetButton("Fire1"))
+				{
+					animator.SetBool("Is_attacking", true);
+					attackTime = startTimeAttack;
+				}
+			}
+			//timer starts. when attack time is less than .33 destroy any objects in attackLocation collider
+			if (attackTime > 0)
+			{
+				attackTime -= Time.deltaTime;
+				if (attackTime <= .33)
+				{
+					//AREA FOR ARROW SHOOTING CODE
+				}
+				//sets attacking bool to false once attack timer is back to zero
+				if (attackTime <= 0)
+				{	
+					attackTime = 0;
+					animator.SetBool("Is_attacking", false);	
+				}
+			}
 		}
 	}
 
